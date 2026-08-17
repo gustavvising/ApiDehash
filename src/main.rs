c92d1a1336dde6cb0c689f64749715e5118f0082
Gustav <gusvih251@edunet.oru.se>
2026-08-17 10:42:37 +0200

    Implement hash library file handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
 use std::env;
 use std::process::exit;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::ErrorKind;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if (args.len() < 2) {
+    if args.len() < 2 {
         println!("Specify the target executable");
         exit(1);
     }
 
-    println!("The args: {}", args[1]);
+    let hashes = match File::open("hashes.txt") {
+        Ok(file) => file,
+
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            // hashes.txt does not exist, creating it
+            create_hashes_file()
+        }
+
+        Err(e) => {
+            eprintln!("Could not open hashes.txt: {}", e);
+            exit(1);
+        }
+    };
+
+}
+
+fn create_hashes_file() -> File {
+    let mut file = match File::create("hashes.txt") {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Could not create hashes.txt: {}", e);
+            exit(1);
+        }
+    };
+
+    if let Err(e) = file.write_all(b"Hello, world!") {
+        eprintln!("Could not write to hashes.txt: {}", e);
+        exit(1);
+    }
+
+    file
 }
